e8af1df603ccd4fb31e595daadba602a0e088dd1
Dendy Prtha <90243581+dendy-prtha-tiukdw@users.noreply.github.com>
2023-11-08 18:45:59 +0700

    add more example

---
+++ C-Memory/array-lanjut/CopyArray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void copyArray(int *source, int *destination, int size) {
+    for (int i = 0; i < size; i++) {
+        *(destination + i) = *(source + i);
+    }
+}
+
+int main() {
+    int source[] = {1, 2, 3, 4, 5};
+    int destination[5]; 
+    int size = sizeof(source) / sizeof(source[0]);
+
+    copyArray(source, destination, size);
+
+    printf("Array yang tersalin: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", destination[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
